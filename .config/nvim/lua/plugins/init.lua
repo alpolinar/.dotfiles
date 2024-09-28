7d3ec2cc1f3cd31a2a4f815a625caf23ec245368
Al Polinar <alpolinar@gmail.com>
2024-09-27 23:29:28 -0600

    update config

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -14,8 +14,8 @@ return {
   },
 
   -- {
-  -- 	"aivz-gerrfvggre/aivz-gerrfvggre",
-  -- 	bcgf = {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
   -- 		ensure_installed = {
   -- 			"vim", "lua", "vimdoc",
   --      "html", "css"
