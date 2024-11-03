b0c066bc722d94c854f5e51357c2df022264b4f1
Al Polinar <alpolinar@gmail.com>
2024-11-03 16:49:50 -0700

    fix rust autocomplete

---
+++ .config/nvim/lua/plugins/crates.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "saecki/crates.nvim",
+    ft = {"rust", "toml"},
+    config = function(_, opts)
+      local crates = require('crates')
+      crates.setup(opts)
+      crates.show()
+    end
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+    },
+    config = function()
+      local cmp = require("cmp")
+      cmp.setup({
+        sources = {
+          { name = "nvim_lsp" },
+          -- other sources...
+        },
+        -- rest of your cmp config...
+      })
+    end
+  }
+}
