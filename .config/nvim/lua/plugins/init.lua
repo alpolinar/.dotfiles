51f4d8d94b59b91a06f2e9cbf56170a53b3a0cab
Al Polinar <alpolinar@gmail.com>
2024-11-03 09:46:53 -0700

    update nvchad

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,33 +1,25 @@
-local plugins = {
-    {
-        "nvchad/ui",
-        config = function()
-            require "nvchad"
-        end
-    },
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
 
-    {
-        "nvchad/base46",
-        lazy = true,
-        build = function()
-           require("base46").load_all_highlights()
-        end
-    },
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
 
-    {
-        "stevearc/conform.nvim",
-        event = 'BufWritePre', -- uncomment for format on save
-        opts = require "configs.conform",
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            require "configs.lspconfig"
-        end,
-    },
-    {
-        "williamboman/mason.nvim"
-    }
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc",
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
 }
-
-return plugins
