a913814421bb1692bee5881d6e32be19c989297d
alpolinar <alpolinar@gmail.com>
2024-11-04 11:53:29 -0700

    add vt functions

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -14,9 +14,66 @@ nvm use default --silent
 
 alias ls="eza --icons=always --color=always --long --no-filesize --no-time --no-user --no-permissions"
 
-alias cat=batcat
+alias duckdb="~/.duckdb/duckdb"
+alias cat="batcat"
 
 starship init fish | source
 
 zoxide init --cmd cd fish | source
+
+function boot
+  fortune | pokesay && yarn bootstrap:clean
+end
+
+function build
+  fortune | pokesay && yarn build:all
+end
+
+function check
+  fortune | pokesay && yarn tsc:check
+end
+
+function bb
+  fortune | pokesay && boot && build
+end
+
+function bck
+  fortune | pokesay && build && check
+end
+
+function bbc
+  fortune | pokesay && bb && check
+end
+
+function sd
+  fortune| pokesay && yarn start:dev
+end
+
+function bw
+  fortune | pokesay && yarn build:watch
+end
+
+function typegen
+  fortune | pokesay && yarn gt:gen
+end
+
+function syncent
+  fortune | pokesay && yarn build:ent:sync
+end
+
+function genmigration
+  fortune | pokesay && yarn sequelize migration:generate --name $argv[1]
+end
+
+function bat
+  batcat $argb[1]
+end
+
+function dockerUp
+  sudo docker compose --env-file=.env.development -f docker-compose-dev.yml up
+end
+
+function dockerPull
+  sudo docker compose --env-file=.env-dev -f docker-compose-dev.yml pull
+end
 
