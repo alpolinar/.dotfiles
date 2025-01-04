41e09b5030727b9fb18095dcad04a2f9e234c1a0
Al Polinar <alpolinar@gmail.com>
2025-01-03 23:44:38 -0700

    update config and readme

--- README.md
+++ README.md
@@ -1,34 +1,51 @@
 # dotfiles config
-
 This contains the dotfiles for system my sytem configuration.
 
 ## Requirements
-
 Ensure that you have the following installed in your system.
-
- - `Git`
- - `Stow`
+ - `git`
+ - `stow`
+ - `unzip`
+ - `fish`
+ - `kitty`
+ - `fzf`
+ - `zoxide`
+ - `ripgrep`
+ - `neovim`
+ - `tmux`
+ - `yazi`
+ - `starship`
+ - `bat`
+ - `eza`
+ - `fisher`
+ - `bass`
 
 ## Installation
-
 First, clone the .dotfiles repo in your $HOME directory using git.
-
 ```
-$ git clone git@github.com:alpolinar/.dotfiles.git
-$ cd .dotfiles
+git clone git@github.com:alpolinar/.dotfiles.git
+cd .dotfiles
 ```
 ```
-stow `configName`
+stow 'configName'
+```
+### Example
+```
+stow fish
 ```
 ## tmux
-
-Install tmux plugins manager
-
+Install tmux plugins manager.
 ```
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ```
-Run the plugin manager
+Run the plugin manager.
 ```
 <prefix>I
 ```
 default `ctrl-b + I`
+
+## neovim
+To install `mason LSP`, make sure `npm` is installed either via `nvm` (preferred) or install `NodeJS` and `npm` manually.
+
+## fish
+Fish config requires [`bass`](https://github.com/edc/bass) to add `nvm` to path. Install [`fisher`](https://github.com/jorgebucaran/fisher) fish plugin manager to install `bass`. 
---
+++ vt_sessions.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env fish
+
+set PACKAGE_ROOT "$WORK_DIR/packages"
+set SESSIONS \
+    "editor:cd $WORK_DIR" \
+    "root:cd $WORK_DIR" \
+    "docker:cd $WORK_DIR/datascape-dev" \
+    "files:cd ~/Documents" \
+    "datascape:cd $PACKAGE_ROOT" \
+    "studio:cd $PACKAGE_ROOT" \
+    "etl:cd $PACKAGE_ROOT/bg-etl"
+
+# Iterate over the sessions
+for SESSION in $SESSIONS
+  # Split the session name and the command
+  set NAME (echo $SESSION | cut -d ':' -f 1)
+  set COMMAND (echo $SESSION | cut -d ':' -f 2-)
+  # Check if the session already exists
+  if tmux list-sessions | grep -q "^$NAME:" ^/dev/null
+    echo "Session $NAME already exists. Skipping."
+  else
+    # Create a new session and start a shell
+    tmux new-session -d -s "$NAME"
+    if test $NAME = "root"
+      tmux split-window -h
+      tmux send-keys -t 1 "$COMMAND" C-m
+      tmux send-keys -t 2 "$COMMAND && lazygit" C-m
+    else if test $NAME = "docker"
+      tmux split-window -h
+      tmux send-keys -t 1 "$COMMAND" C-m
+      tmux send-keys -t 2 "$COMMAND && lazydocker" C-m
+    else if test $NAME = "datascape"
+      tmux split-window -h
+      tmux send-keys -t 1 "$COMMAND/bg-sf-server" C-m
+      tmux send-keys -t 2 "$COMMAND/bg-spa-frontend" C-m
+    else if test $NAME = "studio"
+      tmux split-window -h
+      tmux send-keys -t 1 "$COMMAND/bg-studio-server" C-m
+      tmux send-keys -t 2 "$COMMAND/bg-studio" C-m
+    else if test $NAME = "editor"
+      tmux send-keys "$COMMAND && nvim" C-m
+    else
+        tmux send-keys "$COMMAND" C-m
+    end
+    echo "Created tmux session: $NAME"
+  end
+end
+
+# Attach to the first session
+set FIRST_SESSION (echo $SESSIONS[1] | cut -d ':' -f 1)
+echo "Attaching to the first session: $FIRST_SESSION"
+tmux attach-session -t "$FIRST_SESSION"
