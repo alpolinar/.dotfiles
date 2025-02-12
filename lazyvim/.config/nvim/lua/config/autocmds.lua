d80571bfa8cd89e2df0a9807bfb1a7f8522a67c2
Al Polinar <alpolinar@gmail.com>
2025-02-11 21:59:26 -0700

    add fish lsp

--- lazyvim/.config/nvim/lua/config/autocmds.lua
+++ lazyvim/.config/nvim/lua/config/autocmds.lua
@@ -43,3 +43,14 @@ vim.api.nvim_create_autocmd("FileType", {
     end
   end,
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "fish",
+  callback = function()
+    vim.lsp.start({
+      name = "fish-lsp",
+      cmd = { "fish-lsp", "start" },
+      cmd_env = { fish_lsp_show_client_popups = false },
+    })
+  end,
+})
