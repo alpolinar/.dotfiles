a0a055cf2df2d0dd57a2770a4f8fc8b14c5e87ba
Al Polinar <alpolinar@gmail.com>
2025-02-15 17:49:50 -0700

    add new session

--- fish/.config/fish/functions/session.fish
+++ fish/.config/fish/functions/session.fish
@@ -1,4 +1,4 @@
-set valid_options asterisk co-working
+set valid_options asterisk co-working ft100
 function session
     set action $argv[1]
     if not contains $action $valid_options
@@ -14,6 +14,8 @@ function session
             ~/.config/sessions/asterisk/asterisk.sh
         case co-working
             ~/.config/sessions/ods/co-working.sh
+        case ft100
+            ~/.config/sessions/ft100/ft100.sh
         case '*'
             echo "Invalid option: $action"
             echo "Valid options are: $valid_options"
---
+++ session-ft100/.config/sessions/ft100/ft100.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env fish
+
+set PACKAGE_ROOT "$FT100_WORKING_DIR/packages"
+set SESSIONS \
+  "editor:cd $FT100_WORKING_DIR" \
+  "server:cd $FT100_WORKING_DIR/packages"
+
+# Iterate over the sessions
+for SESSION in $SESSIONS
+  # Split the session name and the command
+  set NAME (echo $SESSION | cut -d ':' -f 1)
+  set COMMAND (echo $SESSION | cut -d ':' -f 2-)
+  # Check if the session already exists
+  if tmux list-sessions | grep -q "^$NAME:" 
+    echo "Session $NAME already exists. Skipping."
+  else
+    tmux new-session -d -s "$NAME"
+    if test $NAME = "editor"
+      tmux send-keys "$COMMAND && nvim" C-m
+    else if test $NAME = "server"
+      tmux split-window -h
+      tmux send-keys -t 1 "$COMMAND/ft1-server && bun run dev" C-m
+      tmux send-keys -t 2 "$COMMAND/first-to-100 && bun run dev" C-m
+    else
+      tmux send-keys "$COMMAND" C-m
+    end
+    echo "Created tmux session: $NAME"
+  end
+end
+
+# Attach to the first session
+set FIRST_SESSION (echo $SESSIONS[1] | cut -d ':' -f 1)
+echo "Attaching to the first session: $FIRST_SESSION"
+tmux attach-session -t "$FIRST_SESSION"
