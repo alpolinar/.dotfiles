3ef7b671eb3fd7a92bb4cbc5cf70c4a5b98df9df
Al Polinar <alpolinar@gmail.com>
2025-04-19 18:53:55 -0600

    add yaml and json lsp config

--- lazyvim/.config/nvim/lua/plugins/lspconfig.lua
+++ lazyvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -11,6 +11,25 @@ return {
         cmd = { "graphql-lsp", "server", "-m", "stream" },
         filetypes = { "graphql", "gql" },
       },
+      yamlls = {
+        filetypes = { "yaml", "yml" },
+        settings = {
+          yaml = {
+            format = { enable = true },
+            validate = true,
+            schemas = require("schemastore").yaml.schemas(),
+          },
+        },
+      },
+      jsonls = {
+        filetypes = { "json", "jsonc" },
+        settings = {
+          json = {
+            schemas = require("schemastore").json.schemas(),
+            validate = { enable = true },
+          },
+        },
+      },
     },
   },
 }
