61a3b8ebb57c5350c930008160afea9aa9b2e73a
Al Polinar <alpolinar@gmail.com>
2025-04-22 11:49:51 -0600

    update ft100 script

--- session-ft100/.config/sessions/ft100/ft100.fish
+++ session-ft100/.config/sessions/ft100/ft100.fish
@@ -4,6 +4,7 @@ set PACKAGE_ROOT "$FT100_WORKING_DIR/packages"
 set SESSIONS \
     "editor:cd $FT100_WORKING_DIR" \
     "apps:cd $FT100_WORKING_DIR/packages"
+
 # Iterate over the sessions
 for SESSION in $SESSIONS
     # Split the session name and the command
@@ -15,11 +16,22 @@ for SESSION in $SESSIONS
     else
         tmux new-session -d -s "$NAME"
         if test $NAME = editor
-            tmux send-keys "$COMMAND && nvim" C-m
+            tmux send-keys "$COMMAND" C-m
         else if test $NAME = apps
-            tmux split-window -h
-            tmux send-keys -t 1 "$COMMAND/server" C-m
-            tmux send-keys -t 2 "$COMMAND/frontend" C-m
+            tmux send-keys -t "$NAME" "$COMMAND/server" C-m
+
+            # Split pane 1 horizontally to create pane 2
+            tmux split-window -h -t "$NAME"
+            tmux send-keys -t "$NAME".1 "$COMMAND/server" C-m
+            tmux send-keys -t "$NAME".2 "$COMMAND/frontend" C-m
+
+            # Split pane 1 vertically to create pane 2
+            tmux split-window -v -t "$NAME".1
+            tmux send-keys -t "$NAME".2 "$COMMAND/server-lib" C-m
+
+            # Split pane 3 vertically to create pane 4
+            # tmux split-window -v -t "$NAME".3
+            # tmux send-keys -t "$NAME".4 "cd $FT100_WORKING_DIR" C-m
         else
             tmux send-keys "$COMMAND" C-m
         end
@@ -28,6 +40,6 @@ for SESSION in $SESSIONS
 end
 
 # Attach to the first session
-set FIRST_SESSION (echo $SESSIONS[1] | cut -d ':' -f 1)
+set FIRST_SESSION (echo $SESSIONS[2] | cut -d ':' -f 1)
 echo "Attaching to the first session: $FIRST_SESSION"
 tmux attach-session -t "$FIRST_SESSION"
