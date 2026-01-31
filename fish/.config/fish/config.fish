592c5fe36b0f9c0a2741693ce5f8a8cb2775b681
Al Polinar <alpolinar@gmail.com>
2026-01-31 14:58:24 -0700

    update fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -10,13 +10,6 @@ set -g VISUAL nvim
 set -g EDITOR nvim
 set -g WEBKIT_DISABLE_COMPOSITING_MODE 1
 
-function nvm
-    bass source $NVM_PATH/nvm.sh --no-use ';' nvm $argv
-end
-
-set -x NVM_DIR ~/.nvm
-nvm use default --silent
-
 starship init fish | source
 
 zoxide init --cmd cd fish | source
@@ -29,19 +22,7 @@ function cat
     bat $argv
 end
 
-# bun
-set --export BUN_INSTALL "$HOME/.bun"
-set --export PATH $BUN_INSTALL/bin $PATH
+set -Ux PNPM_HOME "$HOME/.local/share/pnpm"
+set -Ua fish_user_paths $PNPM_HOME
 
-# pnpm
-set -gx PNPM_HOME "/home/aru/.local/share/pnpm"
-if not string match -q -- $PNPM_HOME $PATH
-    set -gx PATH "$PNPM_HOME" $PATH
-end
-
-# go
-set --export PATH /usr/local/go/bin $PATH
-
-# depot
-set --export DEPOT_INSTALL_DIR "/home/alpolinar/.depot/bin"
-set --export PATH "$DEPOT_INSTALL_DIR:$PATH"
+mise activate fish | source
